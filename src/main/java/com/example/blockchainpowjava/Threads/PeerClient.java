7d97bdd04a6b734934d874e6d67f4f7e261c833a
gotenigo <74220442+gotenigo@users.noreply.github.com>
2022-03-15 22:21:20 +0000

    commit

--- src/main/java/com/example/blockchainpowjava/Threads/PeerClient.java
+++ src/main/java/com/example/blockchainpowjava/Threads/PeerClient.java
@@ -14,6 +14,16 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+
+/******************************************
+ *
+ *  Our PeerClient Thread  will cycle through a predetermined list of peer
+ *  and try to share our blockchain  with them. Since we intend to constantly contact other peers
+ *  and share our blockchain, we loop this thread  in while(true) loop. *
+ *
+ ****************************************/
+
+
 public class PeerClient extends Thread {
 
     private Queue<Integer> queue = new ConcurrentLinkedQueue<>();
@@ -33,21 +43,21 @@ public class PeerClient extends Thread {
                    // allows us to declare resources to be used in a try block with the assurance that the resources will be closed after the execution of that block.
 
                 System.out.println("Sending blockchain object on port: " + queue.peek());
-                queue.add(queue.poll()); // rotate the port order
+                queue.add(queue.poll()); // rotate the port order // so  we rotate at each iteration while(true)
                 socket.setSoTimeout(5000);
 
                 ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
                 ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
 
-                LinkedList<Block> blockChain = BlockchainData.getInstance().getCurrentBlockChain();
+                LinkedList<Block> blockChain = BlockchainData.getInstance().getCurrentBlockChain(); // we use the service layer to get the Blockchain object
                 objectOutput.writeObject(blockChain); // Send the Blockchain via the port
 
-                LinkedList<Block> returnedBlockchain = (LinkedList<Block>) objectInput.readObject(); // Read the Blockchain from port
+                LinkedList<Block> returnedBlockchain = (LinkedList<Block>) objectInput.readObject(); // Read the Blockchain from port (the blockchain send by other Miner on the Network)
 
                 System.out.println(" RETURNED BC LedgerId = " + returnedBlockchain.getLast().getLedgerId()  +
                         " Size= " + returnedBlockchain.getLast().getTransactionLedger().size());
 
-                BlockchainData.getInstance().getBlockchainConsensus(returnedBlockchain);
+                BlockchainData.getInstance().getBlockchainConsensus(returnedBlockchain); // we check the Blockchain is valid
                 Thread.sleep(2000);
 
             } catch (SocketTimeoutException e) {
@@ -61,8 +71,8 @@ public class PeerClient extends Thread {
                 queue.add(queue.poll()); // rotate the port and carry on with the next one
             }
 
-
         }
+
 
 
     }
