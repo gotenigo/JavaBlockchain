27ebfe44b6713ae72a85b5c45681ab725eed5f5a
gotenigo <74220442+gotenigo@users.noreply.github.com>
2022-03-17 21:07:10 +0000

    commit

--- src/main/java/com/example/blockchainpowjava/Threads/PeerRequestThread.java
+++ src/main/java/com/example/blockchainpowjava/Threads/PeerRequestThread.java
@@ -35,14 +35,14 @@ public class PeerRequestThread extends Thread {
             ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
 
-            LinkedList<Block> recievedBC = (LinkedList<Block>) objectInput.readObject(); // read from the  InputStream. We should ge a LinkedList<Block>
+            LinkedList<Block> receivedBC = (LinkedList<Block>) objectInput.readObject(); // read from the  InputStream. We should ge a LinkedList<Block>
 
-            log.info("blockId = " + recievedBC.getLast().getblockId()  +
-                    " Size= " + recievedBC.getLast().getTransactionList().size());
+            log.info("blockId = " + receivedBC.getLast().getblockId()  +
+                    " Size= " + receivedBC.getLast().getTransactionList().size());
 
             // The same service layer BlockchainData is used to validate the Blockchain
             // The nodes in the network use the same protocol to detect malicious branch of the chain.
-           objectOutput.writeObject(BlockchainData.getInstance().getBlockchainConsensus(recievedBC)); //  we send back the Blockchain after running it through our consensus validation
+           objectOutput.writeObject(BlockchainData.getInstance().getBlockchainConsensus(receivedBC)); //  we send back the Blockchain after running it through our consensus validation
 
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
